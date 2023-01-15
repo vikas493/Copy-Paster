408209637bd5f35f59f8087ea6467c9b865ceb5f
vikas493 <104519792+vikas493@users.noreply.github.com>
2023-01-15 13:10:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
     APP_ID = 16193102
     API_HASH = "75eadea6904c83b9be6f2114bd45f6ff"
     TG_USER_SESSION = "AQD3Fk4AqmRPuc_WDHMuNd-QGtpeaQQlksaqK9TC5yM5nJgfuvy8aXji9DfU6UaVZJtqgDPliVNZi_venQBcfFWxiSttm-58oboNtG-remFyB9O5lWPDqGjIN24uyM_xeGiBmi-8sImzdCKiB-KMc2j37hc5UM1CUHBm5-3I8yUaayt_ULTT8kXVR2uc0L9zN_ynIRntRXRifZwvDjrtNjHGAd5Ed1lUUDpOT3vvjfdODaVpeNjHlzcI2Fspz4QT7EtujeZWZDO2Kx2AeuH6ABSAiGDK5QTQl2etfZrWUpjM8LD0GWZ3Xg4XJ1of25I0hqUkZd7GGvKgjhCAqDjgWQKDO7ObjAAAAABqqqKMAA"
-    DB_URI = "postgres://axqblmkb:XcuBrZsUKelKADBYwoV6EF-pwOH11-Xl@snuffleupagus.db.elephantsql.com/axqblmkb"
+    DB_URI = "postgres://rkynohls:xHZVYqtUEaLfKqscSHd6ENpb84bHP24g@kashin.db.elephantsql.com/rkynohls"
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
